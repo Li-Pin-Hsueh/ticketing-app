669f1a36833e0bca336ba1bf4602a6e579ff8878
Pinhsueh <pinxue.life@gmail.com>
2022-04-19 21:02:53 +0800

    Fetched data via custom APP Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,34 @@
 // Use global bootstrap in custom <APP>
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Hi there! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  // console.log(Object.keys('appContext: ', appContext));
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  // INVOK LandingPage component
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  // console.log(data);
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
