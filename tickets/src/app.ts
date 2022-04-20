8bfedd6e3acb6c756c3a235fc3806178f5deec85
Pinhsueh <pinxue.life@gmail.com>
2022-04-20 19:33:10 +0800

    CREATE yaml files

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction } from "express";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@pintickets/common";
+
+const app = express();
+// traffic is being proxy to our APP throught ingress nginx
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+
+
+app.all('*',async (req, res, next) => {
+  return next(new NotFoundError);
+})
+
+app.use(errorHandler);
+
+export { app };
