0f28a46c8adf39babb358629daa521eda6bee661
Pinhsueh <pinxue.life@gmail.com>
2022-04-20 17:00:14 +0800

    publish my common lib and use it

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@pintickets/common";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 import { body } from "express-validator";
-import { BadRequestError } from "../errors/bad-request-error";
 import { Password } from "../services/password";
 const router = express.Router();
 
