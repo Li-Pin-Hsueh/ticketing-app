595489baf9aff1da91bccd074db97d6d1a4afe65
Pinhsueh <pinxue.life@gmail.com>
2022-04-21 16:27:01 +0800

    updates some routers

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction } from "express";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 import { errorHandler, NotFoundError, currentUser } from "@pintickets/common";
 
@@ -12,16 +14,18 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
-app.all('*',async (req, res, next) => {
-  return next(new NotFoundError);
-})
+app.all("*", async (req, res, next) => {
+  return next(new NotFoundError());
+});
 
 app.use(errorHandler);
 
