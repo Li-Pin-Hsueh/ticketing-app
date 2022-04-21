3a87e240670b4694ff865edfe77833cb6a3af3ec
Pinhsueh <pinxue.life@gmail.com>
2022-04-21 14:57:36 +0800

    updates

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@pintickets/common";
-
+import { Ticket } from "../models/ticket";
 const router = express.Router();
 
 router.post(
@@ -9,11 +9,22 @@ router.post(
   requireAuth,
   [
     body("title").not().isEmpty().withMessage("Title is requied"),
-    body("price").isFloat({ gt: 0 }).withMessage('Price must be greater than 0'),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
