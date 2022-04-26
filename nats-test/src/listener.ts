d18f50c6dc425d10a62ceaf4cb8721d1de56944b
Pinhsueh <pinxue.life@gmail.com>
2022-04-26 11:21:17 +0800

    refactor some code

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { json } from "stream/consumers";
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -10,34 +12,13 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listening connected to NATS");
 
-  stan.on('close', () => {
-    console.log('NATS connection closed');
+  stan.on("close", () => {
+    console.log("NATS connection closed");
     process.exit();
-  })
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "queue-group-name",
-    // "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
   });
+
+  new TicketCreatedListener(stan).listen();
 });
 
-process.on('SIGINT', () => stan.close());
-process.on('SIGTERM', () => stan.close());
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
