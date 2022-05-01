f740dc5987ab09e9345b4d750bab61c66f41abb2
Pinhsueh <pinxue.life@gmail.com>
2022-05-01 16:10:58 +0800

    Create listeners

---
+++ payments/src/models/order.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { OrderStatus } from "@pintickets/common";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+// property:version will be maintained automatically by mongoose-update-if-current module
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+// this will tell mongoose to not use the default '__v' property, but use 'version'
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+// property id will rename to _id
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+export { Order };
